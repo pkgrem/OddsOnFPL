3c0f2e15b2a6f1be47f5412025303ea2031d31ae
Paddy Keogh-Goode <paddy.goode2@mail.dcu.ie>
2018-01-24 20:30:14 +0000

    Add files via upload
    Edited the result page and incorporated a different part of the API [https://fantasy.premierleague.com/drf/entry/48049/event/24/picks] removing requests from the previous point. https://fantasy.premierleague.com/drf/my-team/48049/
    
    There are some issues with the new source being a different format, have this to compare the two formats.
    https://jsonformatter.curiousconcept.com/

--- result.php
+++ result.php
@@ -3,11 +3,36 @@
 Your name is: <?php echo $_POST['teamid']; ?><br />
 
 <?php
-$json=file_get_contents("https://fantasy.premierleague.com/drf/my-team/<?php echo $_POST['teamid']; ?>"); // requires auth
-$array = json_decode(json_encode($json), True);
+$json=file_get_contents("https://fantasy.premierleague.com/drf/entry/48049/event/24/picks"); // requires auth, though when logging in on the site it doesn't allow me to request.
+// perhaps this https://www.npmjs.com/package/fpl-api-node
 
-print_r($array);
+//$array = json_decode(json_encode($json), True);
+$data =  json_decode($json, true);
+//$arr1 = str_split($json);
+
+
+//print_r($array);
+
+
 ?>
+
+<tr>
+    <td>Element</td>
+    <td>Position</td>
+</tr>
+
+<?PHP
+foreach($data as $key=>$item)
+{
+    ?>
+    <tr>
+        <td><?PHP echo $item['element']; ?></td>
+        <td><?PHP echo $item['position']; ?></td>
+    </tr>
+    <?PHP
+}
+?>
+</table>
 
 </body>
 </html>
