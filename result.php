bb6f8d9c1851fcaf08f5461fd217ecd91e631e05
Paddy Keogh-Goode <paddy.goode2@mail.dcu.ie>
2018-01-25 16:55:26 +0000

    Add files via upload
    Changed the result page to actually work! Huzzah.
    
    Also added some styling to it and to the table of all players. 
    
    In result took the team_id from the post function and entered into the url so that the url would be generated from it. Then used the foreach to get the data only from picks and input it into the table.

--- result.php
+++ result.php
@@ -1,38 +1,72 @@
-<html>
-<body>
-Your name is: <?php echo $_POST['teamid']; ?><br />
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Beautiful</title>
+    <style>
+    #customers {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
 
-<?php
-$json=file_get_contents("https://fantasy.premierleague.com/drf/entry/48049/event/24/picks"); // requires auth, though when logging in on the site it doesn't allow me to request.
-// perhaps this https://www.npmjs.com/package/fpl-api-node
+    #customers td, #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
 
-//$array = json_decode(json_encode($json), True);
-$data =  json_decode($json, true);
-//$arr1 = str_split($json);
+    #customers tr:nth-child(even){background-color: #f2f2f2;}
 
+    #customers tr:hover {background-color: #ddd;}
 
-//print_r($array);
+    #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #242424;
+        color: white;
+    }
+    </style>
+  </head>
+  <body>
+    <!--Your name is: <?php echo $_POST['teamid']; ?><br />-->
+    <?php
 
+        $team_id = $_POST['teamid'];
+        $url = "https://fantasy.premierleague.com/drf/entry/".$team_id."/event/24/picks";
 
-?>
+        //print_r($team_id); // tests to see if the team_id input works.
+        //print_r($url); // prints the url to see if it parses correctly
 
-<tr>
-    <td>Element</td>
-    <td>Position</td>
-</tr>
+        $json=file_get_contents($url); // requires auth, though when logging in on the site it doesn't allow me to request.
+        // perhaps this https://www.npmjs.com/package/fpl-api-node
+        //suggested to use http://docs.guzzlephp.org/en/stable/ instead of file_get_contents
 
-<?PHP
-foreach($data as $key=>$item)
-{
-    ?>
-    <tr>
-        <td><?PHP echo $item['element']; ?></td>
-        <td><?PHP echo $item['position']; ?></td>
-    </tr>
-    <?PHP
-}
-?>
-</table>
+        //$array = json_decode(json_encode($json), True);
+        $data =  json_decode($json, true);
+        //$arr1 = str_split($json);
+        //$ArrayParameters = $data['entry_history']['event']['picks']; // chose the params from the json
 
-</body>
+        //print_r($data['picks']);
+        //var_dump(json_decode($json, true));
+        ?>
+    <table id ="customers">
+        <tr>
+          <th>Player ID</td>
+          <th>Position</td>
+      </tr>
+
+      <?PHP
+      foreach($data['picks'] as $key=>$item)
+      {
+          ?>
+          <tr>
+              <td><?PHP echo $item['element']; ?></td>
+              <td><?PHP echo $item['position']; ?></td>
+          </tr>
+          <?PHP
+      }
+      ?>
+      </table>
+  </body>
 </html>
--- table.php
+++ table.php
@@ -1,17 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#customers {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#customers td, #customers th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #242424;
+    color: white;
+}
+</style>
+</head>
+<body>
 <?php
 $json=file_get_contents("https://fantasy.premierleague.com/drf/elements/");
+//suggested to use http://docs.guzzlephp.org/en/stable/ instead of file_get_contents
+
 $data =  json_decode($json, true);
 ?>
-<table>
+<table id ="customers">
 <tr>
-    <td>ID</td>
-    <td>Web Name</td>
-    <td>Code</td>
-    <td>Full Name</td>
-    <td>Team</td>
-    <td>News</td>
-    <td>Position</td>
-    <td>Current Price</td>
+    <th>ID</td>
+    <th>Web Name</td>
+    <th>Code</td>
+    <th>Full Name</td>
+    <th>Team</td>
+    <th>News</td>
+    <th>Position</td>
+    <th>Current Price</td>
 </tr>
 
 <?PHP
