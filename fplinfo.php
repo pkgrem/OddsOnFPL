bab30bc0e4c2800a3232ce64f5c277d896faf9fa
Paddy Keogh-Goode <paddy.goode2@mail.dcu.ie>
2018-05-29 21:19:44 +0100

    Add files via upload
    Added in JS function to download as CSV

--- fplinfo.php
+++ fplinfo.php
@@ -1,6 +1,50 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <script>
+    function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
+}
+    </script>
 <style>
 #player_info {
     font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
@@ -27,6 +71,7 @@
 </style>
 </head>
 <body>
+<button onclick="exportTableToCSV('fplinfo.csv')">Export HTML Table To CSV File</button>
 <?php
 $json=file_get_contents("https://fantasy.premierleague.com/drf/bootstrap-static");
 //suggested to use http://docs.guzzlephp.org/en/stable/ instead of file_get_contents
@@ -111,3 +156,4 @@ foreach($data['elements'] as $key=>$item)
 }
 ?>
 </table>
+
